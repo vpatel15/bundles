75c5bc4f49a8e2bdd1707286212474d1b324ac22
Vaibhav Patel <Vaibhav Patel>
2017-03-02 03:03:41 +0530

    stockdog commit

---
+++ stochdog/app/scripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stockDogApp
+ * @description
+ * # stockDogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stockDogApp', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
